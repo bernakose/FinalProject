c671fb24ba09d35f2d7aeb34fbcc53ff177baa95
Berna Köse <anreb_55@hotmail.com>
2021-03-04 01:22:24 +0200

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi.";
+        public static string ProductNameInvalid = "Ürün İsmi Geçersiz.";
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string ProductsListed = "Ürünler Listelendi";
+        internal static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir. ";
+        internal static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var.";
+        internal static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
+    }
+}
